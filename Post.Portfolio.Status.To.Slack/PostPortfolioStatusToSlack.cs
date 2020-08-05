48de2c63b427eb19bae58a0bbfffa0a1f252d5b0
Dilan Livera <Dylan_yahampath@yahoo.com>
2020-08-05 23:04:47 +1000

    add http call to post a message

--- Post.Portfolio.Status.To.Slack/PostPortfolioStatusToSlack.cs
+++ Post.Portfolio.Status.To.Slack/PostPortfolioStatusToSlack.cs
@@ -1,16 +1,54 @@
-using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace Post.Portfolio.Status.To.Slack
 {
     public static class PostPortfolioStatusToSlack
     {
-        [FunctionName("PostPortfolioStatusToSlack")]
-        public static void Run([TimerTrigger("* * 9 * * 6")]TimerInfo myTimer, ILogger log)
+        private static HttpClient _httpClient = new HttpClient
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            BaseAddress = new Uri("https://hooks.slack.com")
+        };
+
+        private static string Uri = "/services/T018347L5QQ/B017KBF94TH/HvqdJ8AZFN7VaGOuQ0sUZTaj";
+
+        [FunctionName("PostPortfolioStatusToSlack")]
+        public static async Task<IActionResult> RunAsync(
+            [TimerTrigger("* * 9 * * 6")] TimerInfo myTimer, 
+            ILogger log)
+        {
+            var slackMessage = new Dictionary<string, List<Section>>
+            {
+                {
+                    "blocks", new List<Section>
+                    {
+                        new Section
+                        {
+                            Type = "section",
+                            Text = new Text
+                            {
+                                Type = "mrkdwn",
+                                Message = "*Hello* and _world_."
+                            }
+                        }
+                    }
+                }
+            };
+
+            var request = new HttpRequestMessage(HttpMethod.Post, Uri)
+            {
+                Content = JsonContent.Create(slackMessage)
+            };
+
+            var postResponse = await _httpClient.SendAsync(request);
+
+            return new OkObjectResult(postResponse.StatusCode);
         }
     }
 }
---
+++ Post.Portfolio.Status.To.Slack/Section.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Post.Portfolio.Status.To.Slack
+{
+    public class Section
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("text")]
+        public Text Text { get; set; }
+    }
+}
