6ece7f6365412898aa7494f1393794921866056e
Dilan Waduge <35996237+DilanLivera@users.noreply.github.com>
2020-08-27 22:52:34 +1000

    get stock price from the api (#5)

--- Post.Portfolio.Status.To.Slack/PostSlackMessage.cs
+++ Post.Portfolio.Status.To.Slack/PostSlackMessage.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -14,9 +15,31 @@ namespace Post.Portfolio.Status.To.Slack
 {
     public static class PostSlackMessage
     {
-        private static HttpClient _slackClient = new HttpClient
+        private static readonly HttpClient _stockPriceClient = new HttpClient
         {
-            BaseAddress = new Uri("https://hooks.slack.com")
+            BaseAddress = new Uri("https://www.alphavantage.co")
+        };
+
+        private static Dictionary<string, Stock> stocks = new Dictionary<string, Stock>
+        {
+            {
+                "BRK.B" , new Stock { Code = "BRK.B", ExchnageCode = "NYSE" }
+            },
+            {
+                "INDA" , new Stock { Code = "INDA", ExchnageCode = "BATS" }
+            },
+            {
+                "MCHI" , new Stock { Code = "MCHI", ExchnageCode = "NASDAQ" }
+            },
+            {
+                "SPCE" , new Stock { Code = "SPCE", ExchnageCode = "NYSE" }
+            },
+            {
+                "TER" , new Stock { Code = "TER", ExchnageCode = "NASDAQ" }
+            },
+            {
+                "VYM" , new Stock { Code = "VYM", ExchnageCode = "NYSEARCA" }
+            },
         };
 
         [FunctionName("PostSlackMessage")]
@@ -28,37 +51,29 @@ namespace Post.Portfolio.Status.To.Slack
                 .AddEnvironmentVariables()
                 .Build();
 
-            var slackUri = config["SLACKURI"];
+            var apiKey = config["STOCKPRICEAPIKEY"];
 
-            var slackMessage = new Dictionary<string, List<Section>>
+            List<Task<Stock>> getStockPrice = stocks.Select(async stock =>
             {
+                string uri = $"/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={stock.Key}&apikey={apiKey}";
+                var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+                using var response = await _stockPriceClient.SendAsync(request);
+
+                if(response.IsSuccessStatusCode)
                 {
-                    "blocks", new List<Section>
-                    {
-                        new Section
-                        {
-                            Type = "section",
-                            Text = new Text
-                            {
-                                Type = "mrkdwn",
-                                Message = "*Hello* and _world_."
-                            }
-                        }
-                    }
+                    var content = await response.Content.ReadAsStringAsync();
                 }
-            };
 
-            var request = new HttpRequestMessage(HttpMethod.Post, slackUri)
-            {
-                Content = JsonContent.Create(slackMessage)
-            };
+                return stock.Value;
 
-            var postResponse = await _slackClient.SendAsync(request);
+            }).ToList();
 
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            log.LogInformation($"Slack returned status code: {postResponse.StatusCode}");
+            await Task.WhenAll(getStockPrice);
 
-            return new OkObjectResult(postResponse.StatusCode);
+            log.LogInformation($"Http trigger function executed at: {DateTime.Now}");
+
+            return new OkObjectResult("I'm Done");
         }
     }
 }
---
+++ Post.Portfolio.Status.To.Slack/Stock.cs
@@ -0,0 +1,9 @@
+﻿namespace Post.Portfolio.Status.To.Slack
+{
+    public class Stock
+    {
+        public string Code { get; set; }
+        public string ExchnageCode { get; set; }
+        public double Price { get; set; }
+    }
+}
